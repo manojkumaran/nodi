ee3f5dfed69c933925a7f7160590207df997ba86
Manoj Kumar A <manojkumar.amalraj@staples.com>
2018-01-22 22:01:35 -0500

    persisting post data to file in disk

--- app/routes/feeds.js
+++ app/routes/feeds.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var feedbackData = require('../data/feedback.json');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 router.get('/feeds', (req,res) =>{
     res.json(feedbackData);
@@ -12,6 +13,9 @@ router.use(bodyParser.urlencoded( { extended : false } ));
 
 router.post('/feeds',  (req,res) =>{
     feedbackData.unshift(req.body);
+    fs.writeFile('app/data/feedback.json', JSON.stringify(feedbackData), 'utf8', function(err){
+        console.log(err);
+    });
     res.json(feedbackData);
 });
 
